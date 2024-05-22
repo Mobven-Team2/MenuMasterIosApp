c18de6c5174cd7c289200c85bc36449795da8060
cansuozdizlekli <101900120+cansuozdizlekli@users.noreply.github.com>
2024-05-22 10:50:09 +0200

    initial commit on main branch

---
+++ MenuMasterIosApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MenuMasterIosApp
+//
+//  Created by Cansu Özdizlekli on 5/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello World!")
+    }
+    
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ MenuMasterIosApp/MenuMasterIosAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  MenuMasterIosAppApp.swift
+//  MenuMasterIosApp
+//
+//  Created by Cansu Özdizlekli on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuMasterIosAppApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
