86f01f0c6633576ec52bc04c1e2f7535a5513c6f
cansuozdizlekli <cansuozdizlekli@gmail.com>
2024-05-23 16:14:38 +0200

    add login page textfield validation

--- MenuMasterIosApp/ViewModel/LoginViewModel.swift
+++ MenuMasterIosApp/ViewModel/LoginViewModel.swift
@@ -20,24 +20,47 @@ class LoginViewModel: ObservableObject {
     
     @Published var isAuthenticated: Bool = false
     
-    var email: String = "admin@admin.com"
-    var password: String = "123456"
+    var email: String = ""
+    var password: String = ""
     
     func login() {
-        
-        let defaults = UserDefaults.standard
-        
-        AuthenticationService().login(email: email, password: password) { result in
-            switch result {
-            case .success(let token):
-                defaults.setValue(token, forKey: "access_token")
-                DispatchQueue.main.async {
-                    self.isAuthenticated = true
-                    print(token)
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
+        if validateFields() {
+            let defaults = UserDefaults.standard
+            
+                        AuthenticationService().login(email: email, password: password) { result in
+                            switch result {
+                            case .success(let token):
+                                defaults.setValue(token, forKey: "access_token")
+                                DispatchQueue.main.async {
+                                    self.isAuthenticated = true
+                                    print(token)
+                                }
+                            case .failure(let error):
+                                print(error.localizedDescription)
+                            }
+                        }
+        }else {
+            print("Giriş başarısız! Lütfen geçerli bir e-posta ve şifre girin.")
         }
+        
+    }
+    
+    func validateFields() -> Bool {
+        guard !email.isEmpty, !password.isEmpty else {
+            return false
+        }
+        
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        
+        if !emailPredicate.evaluate(with: email) {
+            return false
+        }
+        
+        if password.count < 6 {
+            return false
+        }
+        
+        return true
     }
 }
