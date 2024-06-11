0eff7d6599d2c670534b1d38dc9e1f1ff6fd3501
cansuozdizlekli <cansuozdizlekli@gmail.com>
2024-06-11 12:12:34 +0300

    added JWT token decoding to retrieve id from login

--- MenuMasterIosApp/ViewModel/LoginViewModel.swift
+++ MenuMasterIosApp/ViewModel/LoginViewModel.swift
@@ -7,26 +7,42 @@
 
 import Foundation
 import SwiftUI
+import JWTDecode
 
 class LoginViewModel: ObservableObject {
     
     @Published var email: String = ""
     @Published var password: String = ""
     @Published var isAuthenticated: Bool = false
+    @Published var userId : String = ""
     
     var loginValidator = Validator()
-
+    
     func login() {
         if validateFields() {
             let defaults = UserDefaults.standard
-
+            
             LoginService().loginUserFunc(email: email, password: password) { result in
                 switch result {
+                    
                 case .success(let token):
                     defaults.setValue(token, forKey: "access_token")
-                    DispatchQueue.main.async {
-                        self.isAuthenticated = true
-                        print(token)
+                    do {
+                        let jwt = try decode(jwt: token)
+                        if let userId = jwt.claim(name: "Id").string {
+                            DispatchQueue.main.async {
+                                self.userId = userId
+                                self.isAuthenticated = true
+                                // Kullanıcının giriş yapmış olduğunu saklıyoruz
+                                defaults.set(true, forKey: "isUserLoggedIn")
+                                defaults.setValue(userId, forKey: "userId")
+                                print("User ID: \(userId)")
+                            }
+                        } else {
+                            print("User ID bulunamadı")
+                        }
+                    } catch {
+                        print("JWT decode hatası: \(error.localizedDescription)")
                     }
                 case .failure(let error):
                     print(error.localizedDescription)
