b90eb66cf6a9960120bd4f8635285efdb8d78d3c
cansuozdizlekli <cansuozdizlekli@gmail.com>
2024-06-13 19:52:22 +0300

    add SuggestionView to RecipeDetailView

--- MenuMasterIosApp/View/Screen/RecipeDetail/RecipeDetailView.swift
+++ MenuMasterIosApp/View/Screen/RecipeDetail/RecipeDetailView.swift
@@ -144,6 +144,8 @@ struct RecipeDetailView: View {
                             }.padding(.vertical,10)
                             
                             RecipeView(recipeText: recipe.recipe).padding(.horizontal,20)
+                            
+                            suggestionView
                             Spacer()
                         }
                         
@@ -182,6 +184,33 @@ extension RecipeDetailView {
                 .frame(width: 20,height: 32)
                 .padding(0)
         }
+    }
+    
+    private var suggestionView : some View {
+        HStack {
+            Image("suggestion-first")
+                .frame(width: 100, height: 100, alignment: .leading)
+            
+            VStack(alignment:.leading) {
+                Text("Püf Noktalar")
+                    .offset(x:-32)
+                    .font(.poppins(size: 14))
+                    .bold()
+                
+                
+                Text("Yemeğinizi pişirirken süreyi dikkatlice takip etmeyi unutmayın")
+                    .frame(width: 240, alignment: .leading)
+                    .font(.poppins(size: 14))
+                    .fontWeight(.regular)
+                    .padding(.leading, -32)
+                    .padding(.trailing, 28)
+                .lineLimit(3)
+            }
+        }
+        .frame(width: UIScreen.main.bounds.width - 48, height: 80)
+        .background(Color.theme.suggestionRedColor)
+        .cornerRadius(16)
+        .padding(.top, 12)
     }
 }
 
